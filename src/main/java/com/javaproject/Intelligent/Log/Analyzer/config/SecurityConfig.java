164ce604a3346cb78c74e116a23331639bcaae15
Yogesh Arivazhagan <yogesharivazhagan@gmail.com>
2026-05-11 12:00:18 +0530

    fix spring security empty credentials

--- src/main/java/com/javaproject/Intelligent/Log/Analyzer/config/SecurityConfig.java
+++ src/main/java/com/javaproject/Intelligent/Log/Analyzer/config/SecurityConfig.java
@@ -3,12 +3,9 @@ package com.javaproject.Intelligent.Log.Analyzer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -16,9 +13,6 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().permitAll()
                 );
